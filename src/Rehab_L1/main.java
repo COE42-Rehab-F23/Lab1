9efc39c18fe05e2b4b90936957e0669a26ed408c
rehabko <g00087657@aus.edu>
2023-09-13 18:03:00 +0400

    Feature B added

--- src/Rehab_L1/main.java
+++ src/Rehab_L1/main.java
@@ -2,6 +2,11 @@ package Rehab_L1;
 
 public class main {
 
+	public void FeatureB()
+	{
+		//Feature B added
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("hello world\n");
 
